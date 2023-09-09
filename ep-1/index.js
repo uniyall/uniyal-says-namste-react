3b569f09f7dbcb2714d2eaa8676d2119a54d9e7f
uniyall <prateekkuniyal@gmail.com>
2023-09-09 13:59:15 +0530

    first

---
+++ ep-1/index.js
@@ -0,0 +1,14 @@
+const heading = React.createElement('h1', {id : 'heading'}, 'The Real Heading!');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
+
+const nestedDivs = React.createElement('div', {}, [
+    React.createElement('div', {id : '1'}, 
+    [React.createElement('h1', {}, 'I am an h1'),
+    React.createElement('h2', {}, 'I am an h2')]), 
+    React.createElement('div', {id : '2'}, 
+    [React.createElement('h1', {}, 'I am an h1'),
+    React.createElement('h2', {}, 'I am an h2')])
+]);
+
+root.render(nestedDivs);

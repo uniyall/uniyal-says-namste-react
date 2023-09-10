fbae1bb92f5ba46d61842a8c2f142a0a05a474cc
uniyall <prateekkuniyal@gmail.com>
2023-09-10 18:17:43 +0530

    folder rename

--- ep-1_2/index.js
+++ ep-1_2/index.js
@@ -7,7 +7,7 @@ root.render(heading);
 
 const nestedDivs = React.createElement('div', {}, [
     React.createElement('div', {id : '1'}, 
-    [React.createElement('h1', {}, 'I am an h1'),
+    [React.createElement('h1', {id : 'top'}, 'I am an h1'),
     React.createElement('h2', {}, 'I am an h2')]), 
     React.createElement('div', {id : '2'}, 
     [React.createElement('h1', {}, 'I am an h1'),
